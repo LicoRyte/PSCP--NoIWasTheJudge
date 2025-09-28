76c8e5c71700334f3cf9cb5122b81df9a54fbeeb
Lico Not A Dev <alternativeclonez@gmail.com>
2025-09-28 12:11:32 +0700

    Ready To Implement Health and Damage Signal

---
+++ Base/health_component.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+@export var max_health: float = 100.0
+var current_health: float
+
+func _ready() -> void:
+	current_health = max_health
+
+func set_max_stamina(amount: float):
+	max_health = amount
+
+func change_stamina(amount: float):
+	current_health += amount
+	current_health = clamp(current_health, 0, max_health)
+
+func use_stamina(amount: float):
+	if current_health <= 0 or amount > current_health:
+		return false
+	else:
+		return true
+	
