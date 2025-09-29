be35460322f80188a617bfa628eb1f3e5f66b673
Lico Not A Dev <alternativeclonez@gmail.com>
2025-09-29 08:30:34 +0700

    Base Hitboxes
    ready to implement hitboxes for both players and enemies

--- Service/GameEvents.gd
+++ Service/GameEvents.gd
@@ -2,11 +2,3 @@ extends Node
 
 signal _hpchanged(amount: float)
 signal _player_died
-
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
