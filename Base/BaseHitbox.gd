2bfcdbbb3ea94d39c8955031b15f76df476c361c
Lico Not A Dev <alternativeclonez@gmail.com>
2025-09-29 21:25:54 +0700

    Hitbox Implement

--- Base/BaseHitbox.gd
+++ Base/BaseHitbox.gd
@@ -6,10 +6,19 @@ var collision_owner: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	collision_owner = get_parent()
-	collision_shape = get_node_or_null("Hitbox")
-
+	if self.get_parent() is Node2D:
+		collision_owner = get_parent()
+	if self.get_children():
+		collision_shape = get_child(0)
+	if not self.is_connected("body_entered", _on_body_entered):
+		connect("body_entered", _on_body_entered)
+		print_debug("connect")
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == collision_owner:
+		return
