984447e8382bb0bc77af263dd8c02cbab9c09cd9
Gdza4826 <112715972+Gdza4826@users.noreply.github.com>
2025-10-01 15:18:43 +0700

    hello

--- Scripts/Player State/idle.gd
+++ Scripts/Player State/idle.gd
@@ -5,13 +5,13 @@ extends State
 @export var killed_state: State
 @export var stunned_state: State
 
-var move_speed = 10
-var acceleration = 60
+
 func enter():
 	pass
 func exit():
 	pass
 func process_physics(delta: float) -> State:
+
 	if Input.is_action_just_pressed("Space"):
 		return jump_state
 	if player.is_died:
@@ -19,6 +19,17 @@ func process_physics(delta: float) -> State:
 		
 	#player.velocity.x = lerp(player.velocity.x , 40, delta)
 	player.move_and_slide()
+
+	if player.is_died:
+		return killed_state
+
 	return null
+
 func process_input(event: InputEvent) -> State:
+	var input_direction = Vector2(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		Input.get_action_strength("Up") - Input.get_action_strength("Down")
+	)
+	if input_direction:
+		return run_state
 	return null
--- Scripts/Player State/run.gd
+++ Scripts/Player State/run.gd
@@ -5,12 +5,21 @@ extends State
 @export var killed_state: State
 @export var stunned_state: State
 
-var move_speed =400
+var move_speed = 400
+var acceleration = 60
+
 func enter():
 	pass
 func exit():
 	pass
 func process_physics(delta: float) -> State:
+	var input_direction = Vector2(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	)
+	player.velocity = lerp(player.velocity, input_direction * move_speed, delta * acceleration)
+	player.move_and_slide()
+
 	if player.is_died:
 		return killed_state
 	return null
