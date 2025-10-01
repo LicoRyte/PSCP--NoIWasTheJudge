be0e33953e6aa2801493995e412d6d751219d2a8
Gdza4826 <112715972+Gdza4826@users.noreply.github.com>
2025-10-01 15:38:23 +0700

    sdfghjkl;

--- Scripts/Player State/jump.gd
+++ Scripts/Player State/jump.gd
@@ -6,12 +6,15 @@ extends State
 @export var killed_state: State
 @export var stunned_state: State
 
+var speed_jump = 20
 
 func enter():
+	player.velocity.y = speed_jump
 	pass
 func exit():
 	pass
 func process_physics(delta: float) -> State:
+	
 	player.move_and_slide()
 	if player.is_died:
 		return killed_state
