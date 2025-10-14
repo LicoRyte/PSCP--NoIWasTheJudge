32eeb7772b9bb7e7ccd72ecd6fb1952791ef27f0
PMIPX <phedanatesilaarj@gmail.com>
2025-10-14 10:10:02 +0700

    new

---
+++ Scene/healtBar.gd
@@ -0,0 +1,11 @@
+extends ProgressBar
+
+@export var player: Player
+
+func _ready():
+	player.health.changed.connect(update)
+	update()
+
+func update():
+	value = player.current_health * 100 / player.max_health
+	
