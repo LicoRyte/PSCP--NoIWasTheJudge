d0c97198b83a13ce77592c0e7db13751c5c2f740
PMIPX <phedanatesilaarj@gmail.com>
2025-10-14 16:11:30 +0700

    Pe

--- Scene/main_menu.gd
+++ Scene/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_Start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scene/test.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Scene/Player.tscn") # Replace with function body.
 
 
 func _on_Setting_button_2_pressed() -> void:
