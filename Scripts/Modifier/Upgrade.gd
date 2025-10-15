35e0f16f9125293a6729feb171f7200c6235b0be
Panxp <152055816+txe1750@users.noreply.github.com>
2025-10-15 11:04:41 +0700

    complete Upgrade gun script

---
+++ Scripts/Modifier/Upgrade.gd
@@ -0,0 +1,13 @@
+extends BulletModifier 
+
+class_name Upgrade
+
+
+func _ready() -> void:
+	pass
+
+func on_spawn(bullet: Bullet):
+	bullet.extra_damage += 7
+
+func on_active(bullet, delta):
+	bullet.base_bullet_speed += 100 * delta
