f88480b66d7169b6c32131e07066bd778c524da6
Crimpo <guideandguide2@gmail.com>
2025-10-22 18:36:09 +0700

    file organization
    22/10/2568/18:36

---
+++ Scripts/Boss_scene/Chotibot.gd
@@ -0,0 +1,101 @@
+extends Entity
+class_name Chotibot
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+enum BossStage {  
+	IDLE,      
+	BULLET,  
+	BEAM,
+	STATIC,
+	DEFEATED
+}
+var current_state = BossStage.BEAM
+
+var attack_scene = {
+	"BEAM" : preload("res://Scene/Boss_scene/beam.tscn"),
+	"PSCP" : preload("res://Scene/Boss_scene/pscp.tscn"),
+	"STATIC" : preload("res://Scene/Boss_scene/static.tscn"),
+}
+
+
+var current_bullet_time = 0.0
+var current_stage_time = 0.0
+
+var beam_cooldown = 0.6
+var max_beam_count = 4
+var current_beam = 0
+var current_beam_timer = 0.0
+
+var static_cooldown = 1.4
+var max_static_count = 5
+var current_static = 0
+var current_static_timer = 0.0
+
+var PSCP_cooldown = 0.45
+var max_PSCP_count = 7
+var current_PSCP = 0
+var current_PSCP_timer = 0.0
+
+var destroyed_effect_cooldown = 0.17
+var current_time = 0.0
+
+func _ready() -> void:
+	super._ready()
+	animation_player.play("Idle")
+	
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	if Input.is_action_just_pressed("DevTest"):
+		to(BossStage.BEAM)
+	
+	if current_health <= 0:
+		to(BossStage.DEFEATED)
+	match current_state:
+		
+		BossStage.BEAM:
+			current_beam_timer += delta
+			if current_beam_timer > beam_cooldown:
+				current_beam += 1
+				var attack = attack_scene["BEAM"].instantiate()
+				add_child(attack)
+				attack.global_position = Vector2(0, randf_range(0,360))
+				current_beam_timer = 0.0
+			if current_beam >= max_beam_count:
+				current_beam = 0
+				current_beam_timer = 0.0
+				to(BossStage.STATIC)
+		BossStage.STATIC:
+			current_static_timer += delta
+			if current_static_timer > static_cooldown:
+				current_static += 1
+				var attack = attack_scene["STATIC"].instantiate()
+				add_child(attack)
+				attack.global_position = Vector2(0, 0)
+				current_static_timer = 0.0
+			if current_static >= max_static_count:
+				current_static = 0
+				current_static_timer = 0.0
+				to(BossStage.BULLET)
+		BossStage.BULLET:
+			current_PSCP_timer += delta
+			if current_PSCP_timer > PSCP_cooldown:
+				current_PSCP += 1
+				var attack = attack_scene["PSCP"].instantiate()
+				add_child(attack)
+				attack.global_position = Vector2(0, randf_range(0,360))
+				current_PSCP_timer = 0.0
+			if current_PSCP >= max_PSCP_count:
+				current_PSCP = 0
+				current_PSCP_timer = 0.0
+				to(BossStage.BEAM)
+		BossStage.DEFEATED:
+			current_time += delta
+			if current_time > destroyed_effect_cooldown:
+				CamCom.play_effect("fracture", global_position)
+				GlobalAudio.fx("boss_defeated")
+				current_time = 0.0
+				GameEvents._shake_call.emit()
+
+func to(new_state : BossStage):
+	current_state = new_state
---
+++ Scripts/Service_scripts/CameraComposer.gd
@@ -0,0 +1,54 @@
+extends Node
+
+
+#A Service for Emitting Special Effect and Camera Operator
+@export var random_strength: float = 30.0
+@export var shake_fade: float = 10.0
+
+var rnd := RandomNumberGenerator.new()
+var particle = {
+	"fracture" : preload("res://Asset/Particle/fracture.tscn")
+}
+
+func rotate_cam(camera: Camera2D, angle):
+	var tween = create_tween()
+	tween.tween_property(
+		camera, "rotation", angle, 0.5
+	)
+
+func apply_shake(camera: Camera2D, magnitude: float, lerp_time: float):
+	var strength := magnitude
+	var fade := lerp_time
+
+	# simple coroutine-based shake
+	while strength > 0 and is_instance_valid(camera):
+		strength = lerpf(strength, 0, fade * get_process_delta_time())
+		camera.offset = Vector2(
+			rnd.randf_range(-strength, strength),
+			rnd.randf_range(-strength, strength)
+		)
+		await get_tree().process_frame
+
+	if is_instance_valid(camera):
+		camera.offset = Vector2.ZERO
+
+func play_effect(effect : String, position : Vector2):
+	var part = particle[effect]
+	var part_scene = part.instantiate()
+	get_tree().current_scene.add_child(part_scene)
+	part_scene.global_position = position
+	var actual_particle_node = part_scene.get_node("CPUParticles2D")
+	if actual_particle_node:
+		do_effect(actual_particle_node)
+	
+	await get_tree().create_timer(1).timeout
+	if part_scene:
+		part_scene.queue_free()
+	
+	
+func do_effect(particle_node : CPUParticles2D):
+	particle_node.one_shot = true
+	particle_node.emitting = true	
+	
+	
+	
